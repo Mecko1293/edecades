813b0c4fd67fc964bf33ed3020340b880838316a
Mecko1293 <anthonykittles@outlook.com>
2026-05-17 22:45:38 -0500

    Music: verified artist photos for all 11 decades

--- edecades-rebuild/src/pages/Music.jsx
+++ edecades-rebuild/src/pages/Music.jsx
@@ -5,17 +5,17 @@ const DECADES_ORDER = ['1920s','1930s','1940s','1950s','1960s','1970s','1980s','
 
 // Representative artist/era photos from Wikimedia Commons
 const MUSIC_PHOTOS = {
-  '1920s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Louis_Armstrong_restored.jpg/330px-Louis_Armstrong_restored.jpg',
+  '1920s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Louis_Armstrong_in_Color_%28restored%29.jpg/330px-Louis_Armstrong_in_Color_%28restored%29.jpg',
   '1930s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Benny_Goodman_-_Life.jpg/330px-Benny_Goodman_-_Life.jpg',
   '1940s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Frank_Sinatra_Pal_Joey_1957.jpg/330px-Frank_Sinatra_Pal_Joey_1957.jpg',
-  '1950s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Elvis_Presley_1970.jpg/330px-Elvis_Presley_1970.jpg',
-  '1960s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/The_Beatles_and_Ed_Sullivan.jpg/330px-The_Beatles_and_Ed_Sullivan.jpg',
-  '1970s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Bee_Gees_1979.jpg/330px-Bee_Gees_1979.jpg',
-  '1980s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Michael_Jackson_Dangerous_World_Tour.jpg/330px-Michael_Jackson_Dangerous_World_Tour.jpg',
-  '1990s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Nirvana_around_1992.jpg/330px-Nirvana_around_1992.jpg',
-  '2000s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eminem_Concert_for_Valor_2014.jpg/330px-Eminem_Concert_for_Valor_2014.jpg',
-  '2010s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Taylor_Swift_-_Red_Tour.jpg/330px-Taylor_Swift_-_Red_Tour.jpg',
-  '2020s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/The_Weeknd_2018.jpg/330px-The_Weeknd_2018.jpg',
+  '1950s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Elvis_Presley_promoting_Jailhouse_Rock.jpg/330px-Elvis_Presley_promoting_Jailhouse_Rock.jpg',
+  '1960s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/The_Beatles_1963_Dezo_Hoffman_Capitol_Records_press_photo_2.jpg/330px-The_Beatles_1963_Dezo_Hoffman_Capitol_Records_press_photo_2.jpg',
+  '1970s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Bee_Gees_1977.JPG/330px-Bee_Gees_1977.JPG',
+  '1980s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Michael_Jackson_1983_%283x4_cropped%29_%28contrast%29.jpg/330px-Michael_Jackson_1983_%283x4_cropped%29_%28contrast%29.jpg',
+  '1990s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Nirvana_around_1992_%28cropped%29.jpg/330px-Nirvana_around_1992_%28cropped%29.jpg',
+  '2000s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eminem_2021_Color_Corrected.jpg/330px-Eminem_2021_Color_Corrected.jpg',
+  '2010s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_%283%29.png/330px-Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_%283%29.png',
+  '2020s': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/The_Weeknd_Portrait_by_Brian_Ziff.jpg/330px-The_Weeknd_Portrait_by_Brian_Ziff.jpg',
 };
 
 export default function Music() {
